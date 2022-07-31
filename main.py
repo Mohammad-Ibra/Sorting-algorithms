affbb7e4be5d3dbf0742561668af1333ec9843f4
Mohammad-Backer <58946706+Mohammad-Backer@users.noreply.github.com>
2022-07-31 14:38:06 +0300

    Selection sort

---
+++ main.py
@@ -0,0 +1,10 @@
+from selection import Selection
+
+def main():
+    arr = [64,25,17,34,90,12]
+    s = Selection()
+    s.selection_sort(arr)
+    print(arr)
+
+if __name__ == '__main__':
+    main()
